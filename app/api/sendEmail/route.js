d5fbf5da91195f83c086dec74dea3b8e10d73dfc
Dekin0Linux <dekinlinux@gmail.com>
2024-03-03 16:49:46 +0000

    suspense

---
+++ app/api/sendEmail/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer'
+
+export async function POST(request) {
+    try {
+        const { email, name, msg } = await request.json();
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'dekinlinux@gmail.com',
+                pass: 'fjrvfsvomrhcnhux'
+            }
+        })
+
+        const mailOption = {
+            from: email,
+            to: 'dekinlinux@gmail.com',
+            subject: "Message from Website",
+            html: `
+        <h3>${name} : ${email}</h3>
+       
+        <p>${msg}</> 
+        `
+        }
+
+        await transporter.sendMail(mailOption)
+
+        return NextResponse.json({ message: "Email Sent Successfully" }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to Send Email" }, { status: 500 })
+    }
+}
+
+
+// fjrvfsvomrhcnhux
